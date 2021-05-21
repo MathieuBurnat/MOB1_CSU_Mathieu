d7d3269b6a62dbf69f0acf404b430f991a0c7526
MathieuBurnat <70205357+MathieuBurnat@users.noreply.github.com>
2021-05-21 11:35:27 +0200

    refactor function to class

--- App.js
+++ App.js
@@ -1,33 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View, ImageBackground} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <StatusBar style="auto" />
-      <ImageBackground source={require('./back.jpg')} style={styles.image}>
-        <Text style={styles.text}>WELCOME</Text>
-        
-        <form>
-          <label>
-            <input type="text" name="name" />
-          </label>
-
-          <select>
-            <option value="grapefruit">Pamplemousse</option>
-            <option value="lime">Citron vert</option>
-            <option value="coconut">Noix de coco</option>
-            <option value="mango">Mangue</option>
-          </select>
-
-          <input type="submit" value="Envoyer" />
-        </form>
-
-      </ImageBackground>
-    </View>
-  );
+class app extends Component {
+  render(){
+    return (
+      <View style={styles.container}>
+        <StatusBar style="auto" />
+        <ImageBackground source={require('./back.jpg')} style={styles.image}>
+          <Text style={styles.text}>WELCOME</Text>
+          
+          <form>
+            <label>
+              <input type="text" name="name" />
+            </label>
+  
+            <select>
+              <option value="grapefruit">Pamplemousse</option>
+              <option value="lime">Citron vert</option>
+              <option value="coconut">Noix de coco</option>
+              <option value="mango">Mangue</option>
+            </select>
+  
+            <input type="submit" value="Envoyer" />
+          </form>
+  
+        </ImageBackground>
+      </View>
+    );
+  }
+  
 }
+
+export default app;
 
 
 const styles = StyleSheet.create({
