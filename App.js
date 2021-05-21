47c52754ca24a076e49fdd1eb1a851528f98bb6e
MathieuBurnat <70205357+MathieuBurnat@users.noreply.github.com>
2021-05-21 11:00:57 +0200

    Add local background & update title

--- App.js
+++ App.js
@@ -2,14 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, ImageBackground} from 'react-native';
 
-const image = { uri: "https://reactjs.org/logo-og.png" };
-
 export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
-      <ImageBackground source={image} style={styles.image}>
-        <Text style={styles.text}>Hello there !</Text>
+      <ImageBackground source={require('./back.jpg')} style={styles.image}>
+        <Text style={styles.text}>WELCOME</Text>
+        
       </ImageBackground>
     </View>
   );
@@ -24,11 +23,10 @@ const styles = StyleSheet.create({
   image: {
     flex: 1,
     resizeMode: "cover",
-    justifyContent: "center"
   },
   text: {
     color: "white",
-    fontSize: 42,
+    fontSize: 36,
     fontWeight: "bold",
     textAlign: "center",
     backgroundColor: "#000000a0"
