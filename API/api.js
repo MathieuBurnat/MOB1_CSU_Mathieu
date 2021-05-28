c400a4d2c5991e672405aee55a7de893de5f7641
MathieuBurnat <70205357+MathieuBurnat@users.noreply.github.com>
2021-05-28 10:38:09 +0200

    add constructor

--- API/api.js
+++ API/api.js
@@ -2,18 +2,8 @@ import React from 'react';
 import axios from 'axios';
 
 class API {
-  state = {
-      data: []
-  }
-
   getDataFromBase(){
-    axios.get("http://127.0.0.1:8000/api/bases")
-    .then(res => {
-      const data = res.data;
-      this.state = data;
-      console.log(this.state);
-    })
-    .catch(error => console.error(`Error : ${error}`))
+    return axios.get("http://127.0.0.1:8000/api/bases");
   }
 }
 
--- component/SelectCities.js
+++ component/SelectCities.js
@@ -5,15 +5,26 @@ import myApi from "../API/api.js"
 
 
 export default class SelectCities extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cities = [],
+    };
+  }
 
   componentDidMount(){
-    myApi.getDataFromBase();
-    const test = myApi.state;
+    myApi.getDataFromBase().then(res => {
+      res => {
+        this.setState({
+          cities: res.data
+        });
+      }
+      console.log(res.data);
+      console.log(state.cities);
+    })
   }
 
   render() {
-    console.log(this.test)
-    
     return (
       <p>test</p>
     )
