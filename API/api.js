891758a453d93f04f7e7a46e25939d0abe270e5c
MathieuBurnat <70205357+MathieuBurnat@users.noreply.github.com>
2021-05-28 08:33:26 +0200

    Transform the api class into simple class

--- API/api.js
+++ API/api.js
@@ -2,32 +2,16 @@ import React from 'react';
 
 import axios from 'axios';
 
-export default class API extends React.Component {
-  state = {
-      data: []
-  }
-
+export default class API {
   getDataFrom(url){
     let data;
     axios.get(url)
     .then(res => {
       data = res.data;
       console.log(data);
-      this.setState({ data });
+
+      return data;
     })
     .catch(error => console.error(`Error : ${error}`))
-  }
-
-  render()
-  {
-    let test;
-    if (this.state.data == null){
-      //test = this.getDataFrom(this.props.url);
-      test = this.props.url;
-    }
-
-    return (
-      <p> {test} </p>
-    )
   }
 }
