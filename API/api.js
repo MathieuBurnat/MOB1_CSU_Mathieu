26eeb65ed596815beab011f8b1e7b4085170918b
MathieuBurnat <70205357+MathieuBurnat@users.noreply.github.com>
2021-05-28 09:59:48 +0200

    add componentDidMount

--- API/api.js
+++ API/api.js
@@ -3,14 +3,11 @@ import React from 'react';
 import axios from 'axios';
 
 class API {
-
   getDataFromBase(){
-    let data;
-    axios.get("http://127.0.0.1:8000/api/bases")
+    return axios.get("http://127.0.0.1:8000/api/bases")
     .then(res => {
-      data = res.data;
+      const data = res.data;
       console.log(data);
-      return data;
     })
     .catch(error => console.error(`Error : ${error}`))
   }
--- component/SelectCities.js
+++ component/SelectCities.js
@@ -5,20 +5,19 @@ import myApi from "../API/api.js"
 
 
 export default class SelectCities extends React.Component {
- 
+  componentDidMount(){
+    const cities = myApi.getDataFromBase();
+  }
+
   render() {
     return (
-      <p> { myApi.getDataFromBase() } </p>
-      )
-
-    //could be return data as object and play with 'em into another component ?
-    /*
-        <>
-        <Cat name="Munkustrap" />
-        <Cat name="Spot" />
-        </>
-
-        Interesting : https://medium.com/@cristi.nord/props-and-how-to-pass-props-to-components-in-react-part-1-b4c257381654#:~:text=React%20%E2%80%94%20Render%20Props,implementing%20its%20own%20render%20logic.
-    */
+      <ul>
+        {
+          ({ cities }) => (
+            <li> {cities.name} </li>
+          )
+        }
+      </ul>
+    )
   }
 }
