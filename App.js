64f4f10b3c6b112a9c8bbf8dff25aaba348c5e35
MathieuBurnat <70205357+MathieuBurnat@users.noreply.github.com>
2021-05-31 13:32:22 +0200

    Remove the gui part

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, ImageBackground, SafeAreaView, TextInput, Button} from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, TextInput, Button} from 'react-native';
 
 import SelectCities from "./component/SelectCities.js"
 
@@ -11,38 +11,22 @@ class app extends Component {
     return (
       <View style={styles.container}>
         <StatusBar style="auto" />
-        <ImageBackground source={require('./back.jpg')} style={styles.image}>
-          <Text style={styles.text}>WELCOME</Text>
+          <Text >WELCOME</Text>
           
           <SafeAreaView>
-          <Text style={styles.text}>Init</Text>
 
-            <TextInput style={styles.textInput}/> 
-          <Text style={styles.text}>Password</Text>
+          <TextInput/> 
+          <Text>Password</Text>
 
-          <TextInput style={styles.textInput}/>
+          <TextInput/>
 
-          <Button
-            title="Press me"
-            color="#f194ff"
-            onPress={() => Alert.alert('Button with adjusted color pressed')}
-          />
+          <Button title="Press me"/>
 
           </SafeAreaView>
-
-          <form>
-            <label>
-              <input type="text" name="name" />
-            </label>
-            <input type="submit" value="Envoyer" />
-          </form>
-
         <SelectCities></SelectCities>
-        </ImageBackground>
       </View>
     );
   }
-  
 }
 
 export default app;
@@ -52,19 +36,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: "column"
-  },
-  image: {
-    flex: 1,
-    resizeMode: "cover",
-  },
-  text: {
-    color: "white",
-    fontSize: 36,
-    fontWeight: "bold",
-    textAlign: "center",
-    backgroundColor: "#000000a0"
-  },
-  textInput: {
-    backgroundColor: "white",
   }
 });
