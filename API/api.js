fd6d322ea990cf57cad5daaf0ef184ebdf543ac5
Mathieu <Mathieu.Burnat@cpnv.ch>
2021-06-14 07:22:51 +0200

    create getMyactionsInShift and add comments

--- API/api.js
+++ API/api.js
@@ -2,10 +2,13 @@ import React from "react";
 import axios from "axios";
 
 class API {
+  // Return cities
+  // This route doesn't need a token
   getCities() {
     return axios.get("http://127.0.0.1:8000/api/bases");
   }
 
+  // Need the user's token
   getReports(token) {
     return axios
       .get("http://localhost:8000/api/reports", {
@@ -15,9 +18,21 @@ class API {
       })
   }
 
+  // Need the user's token
+  // Show my actions into a shift 
   getMyactionsInShift(token, id) {
     return axios
-      .get("http://127.0.0.1:8000/api/myactionsinshift/"+id, {
+      .get("http://127.0.0.1:8000/api/myactionsinshift/" + id, {
+        headers: {
+          Authorization: "Bearer " + token,
+        },
+      })
+  }
+
+  //Get pharmachecks to do on a base
+  getPharmachecks(token, id) {
+    return axios
+      .get("http://127.0.0.1:8000/api/missingchecks/" + id, {
         headers: {
           Authorization: "Bearer " + token,
         },
