5571639b430f63b76b4a9361a3a3ae179a6b7e70
MathieuBurnat <70205357+MathieuBurnat@users.noreply.github.com>
2021-06-14 13:33:37 +0200

    Add start & end date

--- component/report/pharmacheck/pharmacheckList.js
+++ component/report/pharmacheck/pharmacheckList.js
@@ -2,8 +2,9 @@ import React from "react";
 import myApi from "../../../API/api";
 
 import { Text, StyleSheet } from "react-native";
-import { Card, ListItem, Button } from 'react-native-elements'
-import Icon from 'react-native-vector-icons/FontAwesome';
+import { Card, ListItem, Button, View, Input } from "react-native-elements";
+import Icon from "react-native-vector-icons/FontAwesome";
+import NumericInput from 'react-native-numeric-input'
 
 import { FlatList } from "react-native-gesture-handler";
 
@@ -18,30 +19,44 @@ class PharmacheckList extends React.Component {
   }
 
   componentDidMount() {
-    myApi.getPharmachecks(localStorage.getItem("token"), localStorage.getItem("baseId")).then((res) => {
-      this.setState({
-        data: res.data.pharma,
+    myApi
+      .getPharmachecks(
+        localStorage.getItem("token"),
+        localStorage.getItem("baseId")
+      )
+      .then((res) => {
+        this.setState({
+          data: res.data.pharma,
+        });
       });
-    });
   }
 
   renderItem({ item }) {
     let card = (
       <Card>
-        <Text> {item.drug}  </Text>
+        <Text> {item.drug} </Text>
         <Text> Le {item.date} </Text>
+        <Text> Le {item.date} </Text>
+        <Text> {item.start} puis {item.end}</Text>
+        <Input placeholder="0" />
+        <Input placeholder="1" />
+        <NumericInput type='up-down' onChange={value => console.log(value)} />
+
       </Card>
     );
     return card;
   }
 
   render() {
-    return (
-      <FlatList style={styles.items}
+    return this.state.data.length != 0 ? (
+      <FlatList
+        style={styles.items}
         data={this.state.data}
         renderItem={this.renderItem}
         keyExtractor={(item) => item.id}
       />
+    ) : (
+      <Text>Aucuns Pharmacheck sont disponible.</Text>
     );
   }
 }
