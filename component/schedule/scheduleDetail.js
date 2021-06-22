ee629f20a16f40e8425fe2a8bf5e06015a9c9ceb
MathieuBurnat <70205357+MathieuBurnat@users.noreply.github.com>
2021-06-22 11:32:45 +0200

    Save the reason if the user is not OK

--- component/schedule/scheduleDetail.js
+++ component/schedule/scheduleDetail.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { Text, StyleSheet, View } from "react-native";
+import { Text, StyleSheet, View, TextInput } from "react-native";
 import { Card, Button } from "react-native-elements";
 import Icon from "react-native-vector-icons/FontAwesome";
 import { FlatList } from "react-native-gesture-handler";
@@ -12,42 +12,69 @@ class ScheduleDetail extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      item : this.props.item,
       arrayLength: 0,
+      reasonEnabled: false,
+      reason: "",
     };
     this.needToTalkSchedule = this.needToTalkSchedule.bind(this);
     this.saveSchedule = this.saveSchedule.bind(this);
     this.confirmworkplan = this.confirmworkplan.bind(this);
+    this.setReason = this.setReason.bind(this);
+    this.saveReason = this.saveReason.bind(this);
   }
 
-  needToTalkSchedule(i){
+  needToTalkSchedule(i) {
     console.log("We neek to talk.");
-    let item = {confirmation : 0};
-    this.confirmworkplan(item);
+
+    this.setState({ reasonEnabled: true });
+
+    //let item = {confirmation : 0};
+    //this.confirmworkplan(item);
   }
 
-  saveSchedule(i){
-    console.log("OK !");
-    let item = {id: i.id, confirmation : 1, reason: ""};
+  saveReason(){
+    console.log("Reason saved");
+    console.log(this.state.reason);
+    console.log(this.state.item);
+    
+    //Confirmation 0 = I'm not OK
+    let item = {id: this.state.item.id, confirmation: 0, reason: this.state.reason};
     console.log(item);
-
     this.confirmworkplan(item);
   }
 
-  confirmworkplan(item){
-    myApi.confirmworkplan(localStorage.getItem("token"), item).then((res) => {
+  saveSchedule(i) {
+    //Confirmation 1 = OK
+    let item = { id: i.id, confirmation: 1, reason: "" };
+
+    console.log(item);
+    this.confirmworkplan(item);
+  }
+
+  confirmworkplan(item) {
+    myApi.confirmworkplan(localStorage.getItem("token"), item).then(
+      (res) => {
         showMessage({
           message: "Modification enregistrée",
           type: "success",
           duration: 1000,
         });
         console.log(res);
-      }, (error) => {
+      },
+      (error) => {
         showMessage({
-          message: "Impossible de sauvegarder cette modification, veuillez ressayer plus tard. ",
+          message:
+            "Impossible de sauvegarder cette modification, veuillez ressayer plus tard. ",
           type: "danger",
           duration: 5000,
         });
-      });
+      }
+    );
+  }
+
+  setReason(reason) {
+    this.setState({ reason });
   }
 
   render() {
@@ -60,21 +87,34 @@ class ScheduleDetail extends React.Component {
 
         <Text style={styles.title}> {this.props.item.worktime.type} </Text>
 
-        <span>
-          <Icon
-            style={styles.timesIcon}
-            name={"times"}
-            onPress={() => this.needToTalkSchedule(this.props.item)}
-            size={20}
-          />
+        {this.state.reasonEnabled == false ? (
+          <span>
+            <Icon
+              style={styles.timesIcon}
+              name={"times"}
+              onPress={() => this.needToTalkSchedule(this.props.item)}
+              size={20}
+            />
 
-          <Icon
-            style={styles.checkIcon}
-            name={"check"}
-            onPress={() => this.saveSchedule(this.props.item)}
-            size={20}
-          />
-        </span>
+            <Icon
+              style={styles.checkIcon}
+              name={"check"}
+              onPress={() => this.saveSchedule(this.props.item)}
+              size={20}
+            />
+          </span>
+        ) : (
+          <View>
+            <Text> Ajouter une raison : </Text>
+            <TextInput
+              style={{ height: 40 }}
+              placeholder="Ma raison"
+              onChangeText={this.setReason}
+              maxLength={50}
+            />
+            <Button title="Sauvegarder" onPress={this.saveReason} />
+          </View>
+        )}
       </Card>
     );
     return card;
