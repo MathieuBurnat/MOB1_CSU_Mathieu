4424a8bc98bf793b94642bb8578d0d2e9f093ac6
MathieuBurnat <70205357+MathieuBurnat@users.noreply.github.com>
2021-06-22 10:50:35 +0200

    Display number of unworksplan todo

--- screens/schedule/scheduleList.js
+++ screens/schedule/scheduleList.js
@@ -2,11 +2,11 @@ import React from "react";
 import myApi from "../../API/api";
 
 import { Text, StyleSheet, View } from "react-native";
-import { Card } from 'react-native-elements'
+import { Card } from "react-native-elements";
 import { showMessage } from "react-native-flash-message";
 
 import { FlatList } from "react-native-gesture-handler";
-import SheduleDetail from "../../component/schedule/scheduleDetail"
+import SheduleDetail from "../../component/schedule/scheduleDetail";
 
 class ScheduleList extends React.Component {
   constructor(props) {
@@ -37,18 +37,24 @@ class ScheduleList extends React.Component {
   }
 
   renderItem({ item }) {
-    return <SheduleDetail item={item}/>
+    return <SheduleDetail item={item} />;
   }
 
   render() {
     return (
       <View>
         {this.state.data.length != 0 ? (
-          <FlatList style={styles.items}
-            data={this.state.data}
-            renderItem={this.renderItem}
-            keyExtractor={(item) => item.id}
-          />) : (
+          <div>
+            <Text style={styles.title}> Il restes {this.state.data.length} horraires à confirmer. </Text>
+
+            <FlatList
+              style={styles.items}
+              data={this.state.data}
+              renderItem={this.renderItem}
+              keyExtractor={(item) => item.id}
+            />
+          </div>
+        ) : (
           <Text> Aucuns workplans ne doivent être contabilisés ! </Text>
         )}
       </View>
@@ -61,5 +67,10 @@ export default ScheduleList;
 const styles = StyleSheet.create({
   items: {
     width: "100%",
+  },
+  title: {
+    fontSize: "20px",
+    fontWeight: "bold",
+    color: "#5988b2",
   }
 });
